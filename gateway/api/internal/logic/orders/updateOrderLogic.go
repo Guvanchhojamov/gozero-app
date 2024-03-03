c3bba469263feb93bcd5ce378f2a2c1aee179f03
guvanchh <guvanchojamov@gmail.com>
2024-03-03 15:20:02 +0500

    generated api proto

---
+++ gateway/api/internal/logic/orders/updateOrderLogic.go
@@ -0,0 +1,30 @@
+package orders
+
+import (
+	"context"
+
+	"github.com/Guvanchhojamov/gozero-app/gateway/api/internal/svc"
+	"github.com/Guvanchhojamov/gozero-app/gateway/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UpdateOrderLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUpdateOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UpdateOrderLogic {
+	return &UpdateOrderLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UpdateOrderLogic) UpdateOrder(req *types.UpdateOrderReq) (resp *types.UpdateOrderResp, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
--- gateway/services/orders/rpc/internal/repository/db/order.go
+++ gateway/services/orders/rpc/internal/repository/db/order.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	v1 "github.com/Guvanchhojamov/gozero-app/gateway/services/orders/rpc/v1"
+	"github.com/go-errors/errors"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -15,6 +16,48 @@ func NewOrderRepo(db *pgxpool.Pool) *OrderRepo {
 	return &OrderRepo{db: db}
 }
 func (o *OrderRepo) GetOrders(ctx context.Context, input *v1.GetOrdersRequest) (*v1.GetOrdersResponse, error) {
-	fmt.Println("in order db")
-	return nil, nil
+	var orders []*v1.Order
+	query := fmt.Sprintf(`SELECT id,user_id,product_id,price
+									FROM %s`, ordersTable)
+	rows, err := o.db.Query(ctx, query)
+	defer rows.Close()
+	if err != nil {
+		return nil, errors.New(err)
+	}
+	for rows.Next() {
+		var order v1.Order
+		if err = rows.Scan(
+			&order.Id,
+			&order.ProductId,
+			&order.UserId,
+			&order.Price,
+		); err != nil {
+			return nil, errors.New(err)
+		}
+		orders = append(orders, &order)
+	}
+	return &v1.GetOrdersResponse{Orders: orders}, nil
+}
+func (o *OrderRepo) GetOrderById(ctx context.Context, input *v1.GetOrdersRequest) (*v1.GetOrdersResponse, error) {
+	var orders []*v1.Order
+	query := fmt.Sprintf(`SELECT id,user_id,product_id,price
+									FROM %s`, ordersTable)
+	rows, err := o.db.Query(ctx, query)
+	defer rows.Close()
+	if err != nil {
+		return nil, errors.New(err)
+	}
+	for rows.Next() {
+		var order v1.Order
+		if err = rows.Scan(
+			&order.Id,
+			&order.ProductId,
+			&order.UserId,
+			&order.Price,
+		); err != nil {
+			return nil, errors.New(err)
+		}
+		orders = append(orders, &order)
+	}
+	return &v1.GetOrdersResponse{Orders: orders}, nil
 }
